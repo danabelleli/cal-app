7027b9b0e526f8babc4ca9ee45d68406ec953883
dannabelleli <belleli.danna@gmail.com>
2023-04-30 19:38:43 -0700

    added current day code to the js file to display on the page and added aditional html code to complete all the hours necessary for a standart business day

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,12 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+  // Display current day
+  var displayCurrentDay = $('#currentDay');
+  var currentDate = dayjs().format('ddd, MMM D, YYYY h:mm A');
+  displayCurrentDay.text(currentDate);
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
