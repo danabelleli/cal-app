6df14ce307446d064d00ea2253a320f37d3bb0c3
dannabelleli <belleli.danna@gmail.com>
2023-05-02 23:12:59 -0700

    started writing code for saving the context of the text area on a click event

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,8 @@
 // in the html.
 $(function () {
   // Variables
+  var description = $('.description');
+  var btnSave = $('.saveBtn');
   var currentHour = parseInt(dayjs().format('H'));
   console.log(currentHour);
 
@@ -35,6 +37,10 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
+  var userInput = JSON.parse(localStorage.getItem(''));
+
+
   //
   //  // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -42,4 +48,13 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  $(btnSave).on('click', function (event) {
+    var target = $(event.target);
+    alert(target.parent().id);
+    console.log(target);
+  })
+
+
 });
+
